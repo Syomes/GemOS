605446d45fe4f352da6581ddd303e86521a3d125
Seven_Sec <1260493648@qq.com>
2026-05-20 21:01:48 +0800

    perf: reduce EFI's volumn

--- scripts/build-iso.sh
+++ scripts/build-iso.sh
@@ -7,6 +7,7 @@ set -e
 KERNEL_SRC="external/kernel/vmlinuz"
 KERNEL_DST="tmp/isoroot/boot/vmlinuz"
 INITRAMFS_SRC="tmp/isoroot/boot/initramfs"
+GRUB_DIR="tmp/isoroot/boot/grub"
 GRUB_CFG="grub.cfg"
 EFI_IMG="tmp/isoroot/boot/efi.img"
 EFI_EFI="tmp/isoroot/boot/BOOTx64.EFI"
@@ -29,16 +30,24 @@ cp "$KERNEL_SRC" "$KERNEL_DST"
 
 # EFI
 echo "==> Building GRUB EFI..."
-grub-mkstandalone \
+grub-mkimage \
     --format=x86_64-efi \
     --output="$EFI_EFI" \
-    "boot/grub/grub.cfg=$GRUB_CFG" > /dev/null 2>&1
+    --prefix="/boot/grub" \
+    boot linux normal efi_gop efi_uga gfxterm gfxterm_background gfxmenu \
+    part_gpt part_msdos fat ext2 iso9660 search search_fs_uuid search_fs_file \
+    crypto gcry_md5 gcry_sha256 test echo loadenv
+strip --strip-unneeded "$EFI_EFI" 2>/dev/null
 
 echo "==> Building EFI image..."
-dd if=/dev/zero of="$EFI_IMG" bs=1M count=16 2>/dev/null
-mkfs.vfat "$EFI_IMG" > /dev/null
+dd if=/dev/zero of="$EFI_IMG" bs=1M count=10 2>/dev/null
+mkfs.vfat -F 16 "$EFI_IMG" > /dev/null
 mmd -i "$EFI_IMG" ::/EFI ::/EFI/BOOT
 mcopy -i "$EFI_IMG" "$EFI_EFI" ::/EFI/BOOT/BOOTx64.EFI
+
+echo "==> Copying grub.conf..."
+mkdir -p "$GRUB_DIR"
+cp "$GRUB_CFG" "$GRUB_DIR"
 
 # ISO
 echo "==> Building ISO..."
